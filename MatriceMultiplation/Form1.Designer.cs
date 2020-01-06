078de93e6642a63160e65c76c5107a7a7be7b4a5
Yunus Emre Karaköse <yemre05@hotmail.com.tr>
2020-01-06 16:28:01 +0300

    group and re-update old repositories
    group and re-update old repositories

---
+++ MatriceMultiplation/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace MatriceMultiplation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 176);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(285, 35);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 176);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(600, 35);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(222, 176);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(526, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 90);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 253);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MatriceMultiplation/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatriceMultiplation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[,] m1;//1. matris
+        int[,] m2;//2. matris
+        int[,] m3;//ikisinin çarpımı
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oku();
+        }
+
+        void oku()
+        {
+            string[] satir1 = textBox1.Text.Split('\n');//1. matrisin satırları
+            string[] satir2 = textBox2.Text.Split('\n');//2. matrisin satırları
+            int sa1, sa2, su1, su2;//satir sutun sayilari
+            string[] c1 = satir1[0].Split(' ');//1. matrisin sutun sayısı 1. satira göre
+            string[] c2 = satir2[0].Split(' ');//2. matrisin sutun sayısı 1. satıra göre
+
+            sa1 = satir1.Length;
+            sa2 = satir2.Length;
+            su1 = c1.Length;
+            su2 = c2.Length;
+            if(su1==sa2)//1.nin sutun sayısı 2.nin satir sayisina eşite çarpım yapılabilir
+            {
+                m1 = new int[sa1, su1];
+                m2 = new int[sa2, su2];
+                for(int i = 0; i < satir1.Length; i++)
+                {
+                   string[] e1 = satir1[i].Split(' ');//satirdaki sutun elemanlari
+
+                   for(int j=0;j<e1.Length;j++)
+                   {
+                         m1[i, j] = Convert.ToInt32(e1[j]);
+                   }
+                }
+                for(int k=0;k<satir2.Length;k++)
+                {
+                    string[] e2 = satir2[k].Split(' ');
+
+                    for(int l = 0; l < e2.Length; l++)
+                    {
+                        m2[k, l] = Convert.ToInt32(e2[l]);
+                    }
+                }
+                carp(sa1, sa2, su1, su2);
+            }
+           
+            
+        }
+
+
+        void carp(int sa1,int sa2,int su1,int su2)
+        {
+            m3 = new int[sa1, su2];
+
+            for(int i=0;i<sa1;i++)
+            {
+                for(int j=0;j<su2;j++)
+                {
+                    int tamp = 0;
+                    for(int k=0;k<su1;k++)
+                    {
+                        tamp = tamp +( m1[i, k] * m2[k, j]);
+                    }
+                    m3[i, j] = tamp;
+                }
+            }
+            yaz(sa1, su2);
+        }
+
+        void yaz(int sa1,int su2)
+        {
+            textBox3.Clear();
+            for(int i=0;i<sa1;i++)
+            {
+                for(int j=0;j<su2; j++)
+                {
+                    textBox3.Text = textBox3.Text+ m3[i, j].ToString()+" ";
+                }
+                textBox3.Text =textBox3.Text+ "\r\n";
+            }
+        }
+    }
+}
